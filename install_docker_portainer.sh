abdfd3e4523c796b8bb931a2dfa6ecf3c64aaa1a
lasseveenliese <129764721+lasseveenliese@users.noreply.github.com>
2024-05-01 19:33:58 +0200

    Update install_docker_portainer.sh

--- install_docker_portainer.sh
+++ install_docker_portainer.sh
@@ -17,7 +17,7 @@ for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker c
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
-docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+sudo docker volume create portainer_data
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
 echo "Bitte erstelle sofort einen Benutzer für Portainer unter https://localhost:9443"
